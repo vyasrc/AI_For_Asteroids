2340e38ad73f9336549c766ae5612f0cfbb7e855
Shreyash Mohatta <shreyashmohatta@ShreyashsMBP14.lan>
2022-04-30 13:46:34 -0400

    fixed code for collecting powerup

--- main.py
+++ main.py
@@ -117,7 +117,6 @@ class Orientation(SteeringBehavior):
         self.radius = 5.0
         self.targetRotation = 0.0
         self.rotationSize = 0.0
-        self.rotationSize = 0.0
         self.maxRotation = 180.0
 
     def calculate_acceleration(self, character, goal):
@@ -178,8 +177,8 @@ class Player(object):
         win.blit(self.rotatedSurf, self.rotatedRect)
 
     def turnLeft(self):
-        self.angle += 2
-        # print(self.angle)
+        self.angle += 10
+        self.angle %= 360
         self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
         self.rotatedRect = self.rotatedSurf.get_rect()
         self.rotatedRect.center = (self.x, self.y)
@@ -188,13 +187,15 @@ class Player(object):
         self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
 
     def turnRight(self):
-        self.angle -= 2
+        self.angle -= 10
+        self.angle %= 360
         self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
         self.rotatedRect = self.rotatedSurf.get_rect()
         self.rotatedRect.center = (self.x, self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
+
 
     def moveForward(self):
         self.x += self.cosine * 6
@@ -227,10 +228,15 @@ class Player(object):
             self.y = 0
 
     def updateValues(self, values):
-
         self.angle = values.orientation
         self.x = values.position.x
         self.y = values.position.y
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
 
 
 class Bullet(object):
@@ -400,7 +406,7 @@ def collision_distance(px, py, ax, ay):
     return math.sqrt(math.pow(ax - px, 2) + math.pow(py - ay, 2))
 
 
-def collision_avoidance():
+def collision_avoidance(count):
     # print(len(asteroids))
     # print(player.x, player.y)
 
@@ -408,42 +414,68 @@ def collision_avoidance():
 
     for asteroid in asteroids:
         # print(distance(player.x, player.y, asteroid.x, asteroid.y))
-        if collision_distance(player.x, player.y, asteroid.x, asteroid.y) < 100:
-            # print("Asteroid Nearby")
-            if asteroid.xv > 0 and asteroid.yv > 0:
-                player.turnRight()
-                player.moveForward()
-            elif asteroid.xv > 0 and asteroid.yv < 0:
-                player.turnLeft()
-                player.moveForward()
-            elif asteroid.xv < 0 and asteroid.yv > 0:
-                player.turnLeft()
-                player.moveForward()
+        if collision_distance(player.x, player.y, asteroid.x, asteroid.y) < 150:
+            player_angle = player.angle % 360;
+            print("avoiding collision")
+            if asteroid.xv >= 0 and asteroid.yv >= 0:
+                # print(f'Asteroid vel: {asteroid.xv}, {asteroid.yv} turning right')
+                if(0 <= player_angle <= 90 or 180 <= player_angle <= 270):
+                    player.turnRight()
+            elif asteroid.xv >= 0 and asteroid.yv < 0:
+                # print(f'Asteroid vel: {asteroid.xv}, {asteroid.yv} turning left')
+                if(90 <= player_angle <= 180 or 270 <= player_angle <= 359):
+                    player.turnLeft()
+            elif asteroid.xv < 0 and asteroid.yv >= 0:
+                # print(f'Asteroid vel: {asteroid.xv}, {asteroid.yv} turning left')
+                if(90 <= player_angle <= 180 or 270 <= player_angle <= 359):
+                    player.turnLeft()
             else:
-                player.turnRight()
-                player.moveForward()
-
+                # print(f'Asteroid vel: {asteroid.xv}, {asteroid.yv} turning right')
+                if(0 <= player_angle <= 90 or 180 <= player_angle <= 270):
+                    player.turnRight()
+            if(count%2==0): player.moveForward()
             collision = True
 
     return collision
 
+def collect_powerupV2(star, count):
+
+    angle = (270 - (math.atan2( (star.y + star.h // 2) - player.y, (star.x + star.w // 2) - player.x ) * ( 180 / 3.141592 ) % 360)) % 360
+    
+    # print(f'count : {count} player angle : {player.angle % 360} angle to star {angle}')
+    # print(f'player pos : {player.x}, {player.y} star pos : {star.x}, {star.y}')
+
+    val = player.angle - angle
+    if(abs(val) > 10):
+        if(count % 10 == 0): 
+            player.turnLeft()
+    elif(count % 2 == 0):
+        player.moveForward()
 
 def collect_powerup(time_elapsed):
     # result_position = SteeringData()
     # result_orientation = SteeringData()
 
     result_position = pos.calculate_acceleration(player_kinematic, star_kinematic)
+
     result_orientation = orient.calculate_acceleration(player_kinematic, star_kinematic)
+    # print(f'star angle {star_kinematic.orientation} | player angle {player_kinematic.orientation}')
     # print(resultPosition.linear)
 
     if time_elapsed == 0:
         time_elapsed = clock.get_rawtime()
     if 0 < clock.get_rawtime() < 20:
         time_elapsed = clock.get_rawtime()
-
-    player_kinematic.update(result_position, time_elapsed / 1000)
+    
+    # if(-10 < star_kinematic.orientation - player_kinematic.orientation < 10):
+    #     player_kinematic.update(result_position, time_elapsed / 1000)
+    # else :
+    #     print("rotating")
+    #     player_kinematic.update(result_orientation, time_elapsed / 1000)
+        
     player_kinematic.update(result_orientation, time_elapsed / 1000)
-
+    player_kinematic.update(result_position, time_elapsed / 1000)
+    
     if math.isnan(player_kinematic.orientation):
         player_kinematic.orientation = 0
     # print(player_kinematic.position)
@@ -472,47 +504,20 @@ while run:
     clock.tick(100)
 
     if not gameover:
+
+        collecting_powerup = False
         if count % 50 == 0:
             ran = random.choice([1, 1, 1, 2, 2, 3])
             asteroids.append(Asteroid(ran))
-        if count % 1000 == 0 and stars == [] and not rapidFire:
+        if count % 1000 == 0 and count > 0 and stars == [] and not rapidFire:
             stars.append(Star())
             # collect_star = True
             theta = math.atan2(stars[-1].y - player.y, stars[-1].x - player.x)
             deg = theta * 180 / 3.141592
             # print(deg)
             star_kinematic = Kinematic(pymath.Vector2(stars[-1].x, stars[-1].y), deg)
-        # if count % 750 == 0:
-        #     aliens.append(Alien())
-        # for i, a in enumerate(aliens):
-        #     a.x += a.xv
-        #     a.y += a.yv
-        #     if a.x > sw + 150 or a.x + a.w < -100 or a.y > sh + 150 or a.y + a.h < -100:
-        #         aliens.pop(i)
-        #     if count % 60 == 0:
-        #         alienBullets.append(AlienBullet(a.x + a.w // 2, a.y + a.h // 2))
-        #
-        #     for b in playerBullets:
-        #         if (a.x <= b.x <= a.x + a.w) or a.x <= b.x + b.w <= a.x + a.w:
-        #             if (a.y <= b.y <= a.y + a.h) or a.y <= b.y + b.h <= a.y + a.h:
-        #                 aliens.pop(i)
-        #                 if isSoundOn:
-        #                     bangLargeSound.play()
-        #                 score += 50
-        #                 break
-        #
-        # for i, b in enumerate(alienBullets):
-        #     b.x += b.xv
-        #     b.y += b.yv
-        #     if (
-        #             player.x - player.w // 2 <= b.x <= player.x + player.w // 2) or player.x - player.w // 2 <= b.x + b.w <= player.x + player.w // 2:
-        #         if (
-        #                 player.y - player.h // 2 <= b.y <= player.y + player.h // 2) or player.y - player.h // 2 <= b.y + b.h <= player.y + player.h // 2:
-        #             lives -= 1
-        #             alienBullets.pop(i)
-        #             break
 
-        collision_flag = collision_avoidance()
+        collision_flag = collision_avoidance(count)
 
         player.updateLocation()
 
@@ -525,16 +530,23 @@ while run:
             a.x += a.xv
             a.y += a.yv
             # print(a.xv, a.yv)
-            if (player.x - player.w // 2 <= a.x <= player.x + player.w // 2) or (
-                    player.x + player.w // 2 >= a.x + a.w >= player.x - player.w // 2):
-                if (player.y - player.h // 2 <= a.y <= player.y + player.h // 2) or (
-                        player.y - player.h // 2 <= a.y + a.h <= player.y + player.h // 2):
-                    lives -= 1
-                    asteroids.pop(asteroids.index(a))
-                    if isSoundOn:
-                        bangLargeSound.play()
-                    break
-
+            # if (player.x - player.w // 2 <= a.x <= player.x + player.w // 2) or (
+            #         player.x + player.w // 2 >= a.x + a.w >= player.x - player.w // 2):
+            #     if (player.y - player.h // 2 <= a.y <= player.y + player.h // 2) or (
+            #             player.y - player.h // 2 <= a.y + a.h <= player.y + player.h // 2):
+            #         # print("Collision")
+            #         # print(f'Asteroid vel: {a.xv}, {a.yv}')
+            #         lives -= 1
+            #         asteroids.pop(asteroids.index(a))
+            #         if isSoundOn:
+            #             bangLargeSound.play()
+            #         break
+            if (collision_distance(player.x, player.y, a.x + a.w//2, a.y + a.h//2) < a.w//2):
+                # lives -= 1
+                asteroids.pop(asteroids.index(a))
+                if isSoundOn:
+                    bangLargeSound.play()
+                break
             # bullet collision
             for b in playerBullets:
                 if (a.x <= b.x <= a.x + a.w) or a.x <= b.x + b.w <= a.x + a.w:
@@ -592,23 +604,43 @@ while run:
             # for b in playerBullets:
             #     if (s.x <= b.x <= s.x + s.w) or s.x <= b.x + b.w <= s.x + s.w:
             #         if (s.y <= b.y <= s.y + s.h) or s.y <= b.y + b.h <= s.y + s.h:
+            player_kinematic.position = pymath.Vector2(player.x, player.y)
+            player_kinematic.orientation = player.angle % 360
+
+            theta = math.atan2(s.y - player.y, s.x - player.x)
+            deg = theta * 180 / 3.141592
+            star_kinematic.orientation = deg % 360
+
+            # if argv[1] == 'ARBITRATION' and not collision_flag:
+            #     collect_powerup(timeElapsed)
+            # elif argv[1] == 'BLENDING':
+            #     collect_powerup(timeElapsed)
+
+            collecting_powerup = True
 
             if argv[1] == 'ARBITRATION' and not collision_flag:
-                collect_powerup(timeElapsed)
+                collect_powerupV2(s, count)
             elif argv[1] == 'BLENDING':
-                collect_powerup(timeElapsed)
+                collect_powerupV2(s, count)
 
-            # if count == 101:
-            #     gameover = True
-            if (player.x - player.w // 2 <= s.x <= player.x + player.w // 2) or (
-                    player.x + player.w // 2 >= s.x + s.w >= player.x - player.w // 2):
-                if (player.y - player.h // 2 <= s.y <= player.y + player.h // 2) or (
-                        player.y - player.h // 2 <= s.y + s.h <= player.y + player.h // 2):
-                    rapidFire = True
-                    rfStart = count
-                    stars.pop(stars.index(s))
-                    # playerBullets.pop(playerBullets.index(b))
-                    break
+            # # if count == 101:
+            # #     gameover = True
+            # if (player.x - player.w // 2 <= s.x <= player.x + player.w // 2) or (
+            #         player.x - player.w // 2 >= s.x + s.w >= player.x + player.w // 2):
+            #     if (player.y - player.h // 2 <= s.y <= player.y + player.h // 2) or (
+            #             player.y - player.h // 2 <= s.y + s.h <= player.y + player.h // 2):
+            #         rapidFire = True
+            #         rfStart = count
+            #         stars.pop(stars.index(s))
+            #         # playerBullets.pop(playerBullets.index(b))
+            #         break
+
+            if (collision_distance(player.x, player.y, s.x + s.w//2, s.y + s.h//2) < 20 ):
+                rapidFire = True
+                rfStart = count
+                stars.pop(stars.index(s))
+                # playerBullets.pop(playerBullets.index(b))
+                break
 
         if lives <= 0:
             gameover = True
@@ -644,10 +676,11 @@ while run:
         if not gameover:
             if count % 180 == 0:
                 rand_dir = not rand_dir
-            if rand_dir:
-                player.turnLeft()
-            else:
-                player.turnRight()
+            if count % 5 == 0 and not collecting_powerup and not collision_flag:
+                if rand_dir:
+                    player.turnLeft()
+                else:
+                    player.turnRight()
             if rapidFire:
                 if count % 11 == 0:
                     playerBullets.append(Bullet())
